9a1a9a76776f2e377f4a0fcb6a0c2c25e400bca1
ding-co <pangoons@naver.com>
2021-12-21 09:20:23 +0900

    Add ch06 - pb02

---
+++ code/part2/ch06/pb02.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+
+# Other Solution 1.
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         s[:] = s[::-1]
+
+# Other Solution 2.
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         """
+#         Do not return anything, modify s in-place instead.
+#         """
+#         left, right = 0, len(s) - 1
+#         while left < right:
+#             s[left], s[right] = s[right], s[left]
+#             left += 1
+#             right -= 1
