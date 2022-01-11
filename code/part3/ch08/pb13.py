6eb68f9ea760942d4cc4d753a69d581e48cf9b2c
ding-co <pangoons@naver.com>
2022-01-11 14:36:45 +0900

    [UPDATE] ch 09 - pb 13

--- code/part3/ch08/pb13.py
+++ code/part3/ch08/pb13.py
@@ -7,15 +7,11 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         rev = None
         slow = fast = head
-        
         while fast and fast.next:
             fast = fast.next.next
             rev, rev.next, slow = slow, rev, slow.next
-            
         if fast:
             slow = slow.next
-        
         while rev and rev.val == slow.val:
             rev, slow = rev.next, slow.next
-        
         return not rev
